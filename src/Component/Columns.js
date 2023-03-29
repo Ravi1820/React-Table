47f6e16c8990d7d9bac9b6a8348b18a1df1653f7
Ravi1820 <ravi.j@ksnalabs.com>
2023-03-29 11:42:24 +0530

    Your commit

---
+++ src/Component/Columns.js
@@ -0,0 +1,35 @@
+export const COLUMNS= [
+    {
+        Header:"Id",
+        accessor:'id'
+    },
+    {
+        Header:"First Name",
+        accessor:'first_name'
+    }, 
+    {
+        Header:"Last Name",
+        accessor:'last_name'
+    }, 
+    {
+        Header:"Date",
+        accessor:'date'
+    },
+    {
+        Header:"Email",
+        accessor:'email'
+    },
+    {
+        Header:"Gender",
+        accessor:'gender'
+    },
+    // {
+    //     Header:"Age",
+    //     accessor:'age'
+    // },
+    // {
+    //     Header:"Salary",
+    //     accessor:'salary'
+    // },
+
+]
