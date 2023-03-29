a3174c37be57273c3b13e795d6774f8abebc3a57
Ravi1820 <ravi.j@ksnalabs.com>
2023-03-29 13:29:29 +0530

    Your mm

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div>Hi Ravi</div>
+        <div>Hi Ravi Nagu</div>
         <BasicTable/>
       </header>
     </div>
